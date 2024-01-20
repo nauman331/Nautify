817363648806110c9c4faf2bea952376e91f1c01
nomiking3254 <140294436+nomiking3254@users.noreply.github.com>
2024-01-20 14:25:30 +0500

    mongodb connection string

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/nautify");
+mongoose.connect(process.env.CONNECTION_STRING)
+  .then(() => console.log('connected to database'))
+  .catch((err) => console.log(err));
 const plm = require("passport-local-mongoose");
 
 const Schema = mongoose.Schema;
